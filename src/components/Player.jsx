bbf263f3391fa2d75a50e5414a4351e2024feef6
Dungsora04 <tuandung040204@gmail.com>
2024-07-26 10:57:37 +0700

    use mouse to seek time

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -13,6 +13,7 @@ const Player = () => {
     time,
     previous,
     next,
+    seekSong,
   } = useContext(PlayerContext);
 
   return (
@@ -31,7 +32,12 @@ const Player = () => {
             src={assets.shuffle_icon}
             alt=""
           />
-          <img onClick={previous} className="w-4 cursor-pointer" src={assets.prev_icon} alt="" />
+          <img
+            onClick={previous}
+            className="w-4 cursor-pointer"
+            src={assets.prev_icon}
+            alt=""
+          />
           {playStatus ? (
             <img
               onClick={pause}
@@ -48,7 +54,12 @@ const Player = () => {
             />
           )}
 
-          <img onClick={next} className="w-4 cursor-pointer" src={assets.next_icon} alt="" />
+          <img
+            onClick={next}
+            className="w-4 cursor-pointer"
+            src={assets.next_icon}
+            alt=""
+          />
           <img className="w-4 cursor-pointer" src={assets.loop_icon} alt="" />
         </div>
         <div className="flex items-center gap-5">
@@ -57,6 +68,7 @@ const Player = () => {
           </p>
           <div
             ref={seekBg}
+            onClick={seekSong}
             className="w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer"
           >
             <hr
