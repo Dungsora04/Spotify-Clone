2dec1b8d960d6219d9927235e516c0157a9a29a5
Dungsora04 <tuandung040204@gmail.com>
2024-07-26 10:39:53 +0700

    dynamic timer + play song by click item

--- src/context/PlayerContext.jsx
+++ src/context/PlayerContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useRef, useState } from "react";
+import { createContext, useEffect, useRef, useState } from "react";
 import { songsData } from "../assets/assets";
 
 export const PlayerContext = createContext();
@@ -9,7 +9,7 @@ const PlayerContextProvider = (props) => {
   const seekBar = useRef();
 
   const [track, setTrack] = useState(songsData[0]);
-  const [playStatus, setPlayerStatus] = useState(false);
+  const [playStatus, setPlayStatus] = useState(false);
   const [time, setTime] = useState({
     currentTime: {
       second: 0,
@@ -23,13 +23,41 @@ const PlayerContextProvider = (props) => {
 
   const play = () => {
     audioRef.current.play();
-    setPlayerStatus(true);
+    setPlayStatus(true);
   };
 
   const pause = () => {
     audioRef.current.pause();
-    setPlayerStatus(false);
+    setPlayStatus(false);
   };
+
+  const playWithId = async (id) => {
+    await setTrack(songsData[id]);
+    await audioRef.current.play();
+    setPlayStatus(true);
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      audioRef.current.ontimeupdate = () => {
+        seekBar.current.style.width =
+          Math.floor(
+            (audioRef.current.currentTime / audioRef.current.duration) * 100
+          ) + "%";
+
+        setTime({
+          currentTime: {
+            second: Math.floor(audioRef.current.currentTime % 60),
+            minute: Math.floor(audioRef.current.currentTime / 60),
+          },
+          totalTime: {
+            second: Math.floor(audioRef.current.duration % 60),
+            minute: Math.floor(audioRef.current.duration / 60),
+          },
+        });
+      };
+    }, 1000);
+  }, [audioRef]);
 
   const contextValue = {
     audioRef,
@@ -38,11 +66,12 @@ const PlayerContextProvider = (props) => {
     track,
     setTrack,
     playStatus,
-    setPlayerStatus,
+    setPlayStatus,
     time,
     setTime,
     play,
     pause,
+    playWithId,
   };
 
   return (
