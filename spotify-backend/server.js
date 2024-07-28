eb787cc9864573a25f06c776990c5c5742980954
Dungsora04 <tuandung040204@gmail.com>
2024-07-28 09:23:16 +0700

    add 3 album api

--- spotify-backend/server.js
+++ spotify-backend/server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import cors from "cors";
-import 'dotenv/config';
+import "dotenv/config";
 import songRouter from "./src/routes/songRoute.js";
 import connectDB from "./src/config/mongodb.js";
 import connectCloudinary from "./src/config/cloudinary.js";
+import albumRouter from "./src/routes/albumRoute.js";
 
 // app config
 const app = express();
@@ -17,6 +18,7 @@ app.use(cors());
 
 // initializing routes
 app.use("/api/song", songRouter);
+app.use("/api/album", albumRouter);
 
 app.get("/", (req, res) => res.send("API working"));
 
